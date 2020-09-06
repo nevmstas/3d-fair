ec987327728ef3824a7c8a5acdda8ad03f8bf58e
Stas <nevmyvakastas@gmail.com>
2020-09-07 02:39:42 +0600

    added global style file, typed box

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,19 @@
 import React from 'react';
 import './App.css';
-import { Canvas } from 'react-three-fiber'
+import { Canvas} from 'react-three-fiber'
 import { Box } from './Components/Box'
-
+import GlobalStyle from './globalStyle'
 function App() {
   return (
+    <div>
+      <GlobalStyle />
       <Canvas>
         <ambientLight />
         <pointLight position={[10, 10, 10]} />
         <Box position={[-1.2, 0, 0]} />
         <Box position={[1.2, 0, 0]} />
       </Canvas>
+    </div>   
   );
 }
 
--- src/Components/Box.tsx
+++ src/Components/Box.tsx
@@ -1,20 +1,25 @@
-import React, {useState, useRef} from 'react'
+import React, {useState, useRef } from 'react'
 import { useFrame } from 'react-three-fiber'
+import { Vector3 } from 'three'
 
-export function Box(props) {
+type PropsT = {
+  position: Vector3 | [number, number, number]
+}
+
+export const Box: React.FC<PropsT> = ({ position }) => {
     // This reference will give us direct access to the mesh
-    const mesh = useRef()
+    const mesh = useRef<any>()
   
     // Set up state for the hovered and active state
-    const [hovered, setHover] = useState(false)
-    const [active, setActive] = useState(false)
+    const [hovered, setHover] = useState<boolean>(false)
+    const [active, setActive] = useState<boolean>(false)
   
     // Rotate mesh every frame, this is outside of React without overhead
     useFrame(() => (mesh.current.rotation.x = mesh.current.rotation.y += 0.01))
   
     return (
       <mesh
-        {...props}
+        position = {position}
         ref={mesh}
         scale={active ? [1.5, 1.5, 1.5] : [1, 1, 1]}
         onClick={(e) => setActive(!active)}
